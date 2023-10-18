d4f6f64d120d38a76f4a1823e1174b239afd7f33
Akshay Haridas <X255594@one.merckgroup.com>
2023-10-18 12:54:13 +0530

    a = 5000

--- backend.py
+++ backend.py
@@ -6,3 +6,5 @@ print("the multiplied value is:" +str(e))
 print("after the backend was cretaed it was testedabd it was correct hence moving the project to production")
 print("there is certain issue sthat was found out by the business users hence checkign the code here")
 print("the issues with the backend code has been rectifie=d hence the code is being moved gto production again")
+
+a = 5000
