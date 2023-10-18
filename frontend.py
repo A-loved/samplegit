5ae6fe1313722c048ecf6cba29e0b435b8837a7e
Akshay Haridas <X255594@one.merckgroup.com>
2023-10-18 12:28:19 +0530

    the frondend of the project has been cretaed here

---
+++ frontend.py
@@ -0,0 +1,7 @@
+a =int(input("enetr the first number"))
+b =int(input("enter the second number"))
+temp = a
+a = b
+b = temp
+print("result after swapping the first number:" +str(a))
+print("result after swapping the second number:" +str(b))
