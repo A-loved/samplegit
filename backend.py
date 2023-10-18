3f66e75b3d1ef7258d957c700d0f4f39e9c03814
Akshay Haridas <X255594@one.merckgroup.com>
2023-10-18 12:34:29 +0530

    after the backend was tested this code was moved to production

--- backend.py
+++ backend.py
@@ -2,3 +2,5 @@ c = int(input("enter a numebr to multiply"))
 d = int(input("enter a number to multiply with the first number"))
 e = c*d
 print("the multiplied value is:" +str(e))
+
+print("after the backend was cretaed it was testedabd it was correct hence moving the project to production")
